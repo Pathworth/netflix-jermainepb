0f8d3219755ebc5fedb23339dc7dbfe0d8550d33
Pathworth <125113607+Pathworth@users.noreply.github.com>
2026-01-04 03:35:08 -0500

    Update ProfileBanner.tsx

--- src/profilePage/ProfileBanner.tsx
+++ src/profilePage/ProfileBanner.tsx
@@ -1,44 +1,56 @@
-import React, { useEffect, useState } from 'react';
-import './ProfileBanner.css';
-import PlayButton from '../components/PlayButton';
-import MoreInfoButton from '../components/MoreInfoButton';
-import { getProfileBanner } from '../queries/getProfileBanner';
-import { ProfileBanner as ProfileBannerType } from '../types';
+import React, { useEffect, useState } from "react";
+import "./ProfileBanner.css";
+import PlayButton from "../components/PlayButton";
+import MoreInfoButton from "../components/MoreInfoButton";
+import { getProfileBanner } from "../queries/getProfileBanner";
+import { ProfileBanner as ProfileBannerType } from "../types";
 
 const ProfileBanner: React.FC = () => {
-
-
   const [bannerData, setBannerData] = useState<ProfileBannerType | null>(null);
 
   useEffect(() => {
+    let alive = true;
+
     async function fetchData() {
       const data = await getProfileBanner();
-      setBannerData(data);
+      if (alive) setBannerData(data);
     }
+
     fetchData();
+    return () => {
+      alive = false;
+    };
   }, []);
 
-  if (!bannerData) return <div>Loading...</div>;
+  if (!bannerData) return <div className="profile-banner">Loading...</div>;
 
   const handlePlayClick = () => {
-    window.open(bannerData.resumeLink.url, '_blank');
+    if (bannerData.resumeLink?.url) window.open(bannerData.resumeLink.url, "_blank");
   };
 
-  const handleLinkedinClick = () => { 
-    window.open(bannerData.linkedinLink, '_blank');
-  }
+  const handleLinkedinClick = () => {
+    if (bannerData.linkedinLink) window.open(bannerData.linkedinLink, "_blank");
+  };
 
   return (
-    <div className="profile-banner">
+    <div
+      className="profile-banner"
+      style={{
+        backgroundImage: bannerData.backgroundImage?.url
+          ? `url(${bannerData.backgroundImage.url})`
+          : undefined,
+      }}
+    >
       <div className="banner-content">
-        <h1 className="banner-headline" id='headline'>{bannerData.headline}</h1>
-        <p className="banner-description">
-          {bannerData.profileSummary}
-        </p>
+        <h1 className="banner-headline" id="headline">
+          {bannerData.headline}
+        </h1>
+
+        <p className="banner-description">{bannerData.profileSummary}</p>
 
         <div className="banner-buttons">
           <PlayButton onClick={handlePlayClick} label="Resume" />
-          <MoreInfoButton onClick={handleLinkedinClick} label="Linkedin" />
+          <MoreInfoButton onClick={handleLinkedinClick} label="LinkedIn" />
         </div>
       </div>
     </div>
