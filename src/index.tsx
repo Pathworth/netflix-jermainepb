2db9c84fa7bcd1050489fcb0bc0e55c006628efd
Pathworth <125113607+Pathworth@users.noreply.github.com>
2026-01-19 23:09:28 -0500

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,17 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import ErrorBoundary from "./ErrorBoundary";
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
+    </BrowserRouter>
+  </React.StrictMode>
+);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
